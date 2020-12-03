6820be735ac0f81a8800d5f415814195521f8d74
MajesticWaffle <37852215+MajesticWaffle@users.noreply.github.com>
2020-12-03 17:44:57 -0600

    Version 1.3.2
    * Removed unused PNG texture support from TextureLoader.java
    * Removed unused gdx-twl library (Previously used for .PNG support)

--- src/com/thiccindustries/APE2/io/TextureLoader.java
+++ src/com/thiccindustries/APE2/io/TextureLoader.java
@@ -2,7 +2,6 @@
 
 package com.thiccindustries.APE2.io;
 
-import de.matthiasmann.twl.utils.PNGDecoder;
 import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
@@ -12,41 +11,6 @@ import java.nio.ByteBuffer;
 import static org.lwjgl.opengl.GL11.*;
 
 public class TextureLoader {
-
-    //Loads a texture with no fallback, program will crash if the file does not exist
-    public static Texture loadTextureNoFallback(String fileName) throws NullPointerException, IOException {
-        PNGDecoder decoder = new PNGDecoder(TextureLoader.class.getResourceAsStream(fileName));
-        ByteBuffer buffer = ByteBuffer.allocateDirect(4 * decoder.getWidth() * decoder.getHeight());
-
-        decoder.decode(buffer, decoder.getWidth() * 4, PNGDecoder.Format.RGBA);
-
-        //flip the buffer because opengl is weird
-        buffer.flip();
-
-        int id = glGenTextures();
-
-        glBindTexture(GL_TEXTURE_2D, id);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-        //disable garbage filtering
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-
-        //upload image data into texture
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, decoder.getWidth(), decoder.getHeight(), 0, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
-
-        return new Texture(id, decoder.getWidth() / 16, decoder.getHeight() / 16);
-    }
-
-    //Loads a texture with fallback
-    public static Texture loadTexture(String fileName, Texture fallback) {
-        try {
-            return loadTextureNoFallback(fileName);
-        } catch (Exception e) {
-            System.err.println("Texture File: " + fileName + " does not exist, using fallback texture!");
-            return fallback;
-        }
-    }
 
     /*Loads an austin paint texture with no fallback*/
     public static Texture loadTextureAPNoFallback(String fileName) throws IOException {
